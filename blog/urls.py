24807174d6b23461faa416b446fcc73f289f0197
chano <chanhojin94@gmail.com>
2025-08-14 17:14:31 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views  # 현재 폴더(blog)의 views.py 불러오기
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 
